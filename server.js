8607354fbbb4ffa17f23f9473bdc4e3d119219ad
Danish Pathan <danishpathan3252@gmail.com>
2023-09-13 00:41:45 +0530

    Added Url Shortener

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
+const app = express();
+
+mongoose.connect("mongodb://localhost/urlShortener", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+
+// Clear data at startup
+async function clearData() {
+  try {
+    await ShortUrl.deleteMany({});
+    console.log("Previous data cleared.");
+  } catch (error) {
+    console.error("Error clearing data:", error);
+  }
+}
+
+// Call the clearData function at the beginning of your application startup
+clearData();
+
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render("index", { shortUrls: shortUrls });
+});
+
+app.post("/shortUrls", async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+
+  res.redirect("/");
+});
+
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) return res.sendStatus(404);
+
+  shortUrl.clicks++;
+  shortUrl.save();
+
+  res.redirect(shortUrl.full);
+});
+
+app.listen(process.env.PORT || 5000);
